5c64c1dea21ed781a7d8fc56155cc9fa898c2643
Stephen Lloyd <stephenlloyd@Stephens-MacBook-Pro-2.local>
2016-02-19 12:36:58 +0000

    adde test for pdf

--- spec/controllers/image_generator_controller_spec.rb
+++ spec/controllers/image_generator_controller_spec.rb
@@ -5,17 +5,23 @@ require "base64"
 RSpec.describe ImageGeneratorController, type: :controller do
   let(:image_meta_data) {"\{\"name\" : \"Stephen\", \"movie\" : \"hi\", \"img\" : \"something.png\", \"view\": \"/invite_maker/something.png\"\}"}
   let(:encrypted_data){Base64.urlsafe_encode64(image_meta_data)}
-  let(:image_data){double(:image_data, view: "image_generator/invite_maker/classic.png", name: "hello", quality: 50, locals: {"img" => "some_image.png"} )}
+  let(:image_data){double(:image_data, view: "image_generator/default.erb", name: "hello", quality: 50, locals: {"img" => "some_image.png"} )}
 
   it "extracts the data" do
     expect(ImageData).to receive(:decode).with(encrypted_data).and_return(image_data)
     get :show, image: encrypted_data, format: "png"
   end
 
-  it "sends to default view" do
+  it "sends to default view for png" do
     allow(ImageData).to receive(:decode).with(encrypted_data).and_return(image_data)
     get :show, image: encrypted_data, format: "png"
-    expect(response).to render_template("image_generator/invite_maker/classic.png")
+    expect(response).to render_template("image_generator/default.erb")
+  end
+
+  it "sends to default view for pdf" do
+    allow(ImageData).to receive(:decode).with(encrypted_data).and_return(image_data)
+    get :show, image: encrypted_data, format: "pdf"
+    expect(response).to render_template("image_generator/default.erb")
   end
 
   it "assigns the correct locals" do
