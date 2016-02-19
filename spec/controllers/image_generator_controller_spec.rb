f850d96421b898db6cf22cd00d0a02f09a597117
Stephen Lloyd <stephenlloyd@Stephens-MacBook-Pro-2.local>
2016-02-19 12:02:56 +0000

    tested assinged correct instance variable to locals

--- spec/controllers/image_generator_controller_spec.rb
+++ spec/controllers/image_generator_controller_spec.rb
@@ -5,7 +5,7 @@ require "base64"
 RSpec.describe ImageGeneratorController, type: :controller do
   let(:image_meta_data) {"\{\"name\" : \"Stephen\", \"movie\" : \"hi\", \"img\" : \"something.png\", \"view\": \"/invite_maker/something.png\"\}"}
   let(:encrypted_data){Base64.urlsafe_encode64(image_meta_data)}
-  let(:image_data){double(:image_data, view: "image_generator/invite_maker/classic.png", name: "hello", quality: 50, locals: {"img" => "stephen"} )}
+  let(:image_data){double(:image_data, view: "image_generator/invite_maker/classic.png", name: "hello", quality: 50, locals: {"img" => "some_image.png"} )}
 
   it "extracts the data" do
     expect(ImageData).to receive(:decode).with(encrypted_data).and_return(image_data)
@@ -14,7 +14,14 @@ RSpec.describe ImageGeneratorController, type: :controller do
 
   it "sends to default view" do
     allow(ImageData).to receive(:decode).with(encrypted_data).and_return(image_data)
+    get :show, image: encrypted_data, format: "png"
     expect(response).to render_template("image_generator/invite_maker/classic.png")
+  end
+
+  it "assigns the correct locals" do
+    allow(ImageData).to receive(:decode).with(encrypted_data).and_return(image_data)
+    get :show, image: encrypted_data, format: "png"
+    expect(assigns("img")).to eq("some_image.png")
   end
 
 
