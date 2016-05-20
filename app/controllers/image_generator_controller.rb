21ff8c6415df5d3cd691afbc7e1b018c169776b0
Stephen Lloyd <stephenlloyd@Stephens-MacBook-Pro-2.local>
2016-05-20 09:18:29 +0100

    hmm

--- app/controllers/image_generator_controller.rb
+++ app/controllers/image_generator_controller.rb
@@ -4,18 +4,23 @@ class ImageGeneratorController < ApplicationController
   def show
     image_data = ImageData.decode(params[:image])
     image_data.locals.each {|k,v|instance_variable_set("@#{k}", v )}
-
+    
     respond_to do |format|
-        format.pdf do
-          render pdf: image_data.name,
-          template: image_data.view
-        end
+      format.pdf do
+        render pdf: image_data.name,
+        template: image_data.view
+      end
 
       format.png do
           html = render_to_string(:template => image_data.view)
           kit = IMGKit.new(html, :quality => image_data.quality)
           send_data(kit.to_png, :type => "image/png", :disposition => 'inline')
       end
+
+      format.html do
+        render image_data.view
+      end
+
     end
   end
 end
